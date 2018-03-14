a155c6393617ec1789fe9b7ced59e04528059895
Jake Brown <jakebrown.designer@gmail.com>
2018-03-14 16:38:55 +1100

    worked on enemy controller. Enemy gun rotates to point at player.

---
+++ Assets/Our Assets/Scripts/Enemy/enemyWeaponController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyWeaponController : MonoBehaviour
+{
+    [Header("Weapon Variables")]
+    public Weapon weapon;
+    public int damage;
+
+    public GameObject bullet;
+    public GameObject barrel;
+
+    // Use this for initialization
+    void Start()
+    {
+        damage = weapon.weaponDamageAmount;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    
+}
---
+++ Assets/Our Assets/Scripts/Enemy/lookAtPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookAtPlayer : MonoBehaviour
+{
+    public GameObject playerTarget;
+    private Transform playerTransform;
+	// Use this for initialization
+	void Start ()
+    {
+        playerTarget = GameObject.FindGameObjectWithTag("Player");
+        playerTransform = playerTarget.transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       // Vector3 targetPosition = new Vector3(playerTarget.transform.position.x, this.transform.position.y, playerTarget.transform.position.z);
+        this.transform.LookAt(playerTransform);
+    }
+
+    
+}
