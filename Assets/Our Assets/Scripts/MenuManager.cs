53688fefa3fd17db3ffe75f265400acd7952911b
Jake Brown <jakebrown.designer@gmail.com>
2018-03-27 16:47:24 +1100

    menu implemented

---
+++ Assets/Our Assets/Scripts/MenuManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    public GameObject mainMenu;
+    public GameObject controlsMenu;
+
+    public void Start()
+    {
+        mainMenu.SetActive(true);
+        controlsMenu.SetActive(false);
+    }
+
+    public void PressPlay()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void PressControls()
+    {
+        mainMenu.SetActive(false);
+        controlsMenu.SetActive(true);
+
+    }
+
+    public void PressQuit()
+    {
+        Application.Quit();
+    }
+}
