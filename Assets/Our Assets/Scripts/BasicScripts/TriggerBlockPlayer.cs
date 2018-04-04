c170b63d11f34be896f1d2a1cede2fa468e9aee4
Jason Crawford <jasoncrawford.design@gmail.com>
2018-04-04 10:31:02 +1000

    Changes - Gunshot Audio - E interact move up

---
+++ Assets/Our Assets/Scripts/BasicScripts/TriggerBlockPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBlockPlayer : MonoBehaviour {
+
+
+	public GameObject toggleObject;
+
+
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		
+		if (other.tag == "Player")
+		{
+			toggleObject.SetActive (true);
+
+		}
+		
+	}
+
+
+
+}
